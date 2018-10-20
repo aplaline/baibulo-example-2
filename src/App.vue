e8d9bec1a5e7da707dd25ae53ea41c970c5dc385
Matthias Hryniszak <padcom@gmail.com>
2018-10-20 19:00:43 +0200

    TST-1234 Implemented feature

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <h1>Hello, world!</h1>
     <img alt="Vue logo" src="./assets/logo.png">
   </div>
 </template>
+
+<style>
+html, body {
+  text-align: center;
+}
+</style>
