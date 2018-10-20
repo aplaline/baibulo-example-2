dcabee045269ae4a286948f777ecce893d206fd1
Matthias Hryniszak <padcom@gmail.com>
2018-10-20 19:27:27 +0200

    TST-1234 Added counter

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,25 @@
   <div id="app">
     <h1>Hello, world!</h1>
     <img alt="Vue logo" src="./assets/logo.png">
+    <p>{{ counter }}</p>
+    <button @click="increment">Click me!</button>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.counter++
+    }
+  }
+}
+</script>
 
 <style>
 html, body {
